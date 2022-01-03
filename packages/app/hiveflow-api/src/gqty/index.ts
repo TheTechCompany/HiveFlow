0a3a963422801065073cc5b2bc4a09752c2cb611
Ross Leitch <professional.balbatross@gmail.com>
2022-01-03 17:26:06 +1300

    Basic auth

--- packages/app/hiveflow-api/src/gqty/index.ts
+++ packages/app/hiveflow-api/src/gqty/index.ts
@@ -15,7 +15,7 @@ import { generatedSchema, scalarsEnumsHash } from "./schema.generated";
 
 const queryFetcher: QueryFetcher = async function (query, variables) {
   // Modify "/api/graphql" if needed
-  const response = await fetch("/api/graphql", {
+  const response = await fetch("http://localhost:7000/graphql", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -25,6 +25,7 @@ const queryFetcher: QueryFetcher = async function (query, variables) {
       variables,
     }),
     mode: "cors",
+    credentials: 'include'
   });
 
   const json = await response.json();
--- packages/app/hiveflow-backend/src/index.ts
+++ packages/app/hiveflow-backend/src/index.ts
@@ -1,35 +1,68 @@
-import { config } from 'dotenv'
-config()
-
-import express from 'express';
-import neo4j from "neo4j-driver"
-import { Neo4jGraphQL } from "@neo4j/graphql"
-import { graphqlHTTP } from "express-graphql"
-import typeDefs from './schema'
-import resolvers from './resolvers';
-
-(async () => {
-	
-	const driver = neo4j.driver(
-		process.env.NEO4J_URI || "localhost",
-		neo4j.auth.basic(process.env.NEO4J_USER || "neo4j", process.env.NEO4J_PASSWORD || "test")
-	)
+import { config } from "dotenv";
+config();
+import { readFileSync } from 'fs'
+import express from "express";
+import neo4j from "neo4j-driver";
+import { Neo4jGraphQL } from "@neo4j/graphql";
+import { graphqlHTTP } from "express-graphql";
+import typeDefs from "./schema";
+import resolvers from "./resolvers";
+import jwt from "jsonwebtoken";
 
 
-	console.log("RabbitMQ")
+const publicKey = readFileSync("./public.key", 'utf8');
 
+(async (publicKey: string) => {
+  const driver = neo4j.driver(
+    process.env.NEO4J_URI || "localhost",
+    neo4j.auth.basic(
+      process.env.NEO4J_USER || "neo4j",
+      process.env.NEO4J_PASSWORD || "test"
+    )
+  );
 
-	const resolved = await resolvers(driver.session())
+  console.log("RabbitMQ");
 
-	const neoSchema : Neo4jGraphQL = new Neo4jGraphQL({ typeDefs, resolvers: resolved, driver })
+  const resolved = await resolvers(driver.session());
 
-	const app = express()
+  const neoSchema: Neo4jGraphQL = new Neo4jGraphQL({
+    typeDefs,
+    resolvers: resolved,
+    driver,
+  });
 
-	app.use("/graphql", graphqlHTTP({
-		schema: neoSchema.schema,
-		graphiql: true,
-	}))
+  const app = express();
 
-	app.listen('9011')
+  app.use("/graphql", (req, res, next) => {
+    const hiveJwt = req.headers["x-hive-jwt"]?.toString();
 
-})()
+    console.log(req.headers);
+    if (hiveJwt) {
+      const verified = jwt.verify(
+        hiveJwt,
+        publicKey,
+        { algorithms: ["RS256"] }
+      );
+
+	  console.log(verified);
+
+      (req as any).jwt = {
+		  ...(verified as any || {}),
+        id: verified?.sub,
+      };
+      next();
+    } else {
+      res.send({ error: "No JWT" });
+    }
+  });
+
+  app.use(
+    "/graphql",
+    graphqlHTTP({
+      schema: neoSchema.schema,
+      graphiql: true,
+    })
+  );
+
+  app.listen("9011");
+})(publicKey);
