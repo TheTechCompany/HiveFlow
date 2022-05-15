e1573bdc4346d2adac3be1d620f717e6338c5936
Ross Leitch <ross@hexhive.io>
2022-05-16 08:21:52 +1200

    Update schema

--- packages/app/hiveflow-backend/src/schema/schedule.ts
+++ packages/app/hiveflow-backend/src/schema/schedule.ts
@@ -190,7 +190,7 @@ export default (prisma: PrismaClient) => {
                 if(args.where){
                     console.log({where: args.where})
                     if(args.where.id) whereArg = {...whereArg, id: args.where.id};
-                    if(args.where.timeline) whereArg = {...whereArg, timeline: {id: args.where.timeline}};
+                    if(args.where.timeline) whereArg = {...whereArg, timeline: args.where.timeline};
                     if(args.where.startDate_LTE) whereArg.startDate = {lte: args.where.startDate_LTE};
                     if(args.where.endDate_GTE) whereArg.endDate = {gte: args.where.endDate_GTE};
                 }
