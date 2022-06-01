254544cbe66ce8bcf3c4e159b69cd82fc5941cc6
Ross Leitch <ross@hexhive.io>
2022-06-01 20:36:16 +1200

    Add company name

---
+++ packages/app/hiveflow-backend/prisma/migrations/20220601083554_make_hard_fields_optional/migration.sql
@@ -0,0 +1,8 @@
+-- AlterTable
+ALTER TABLE "Equipment" ALTER COLUMN "name" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Estimate" ALTER COLUMN "name" DROP NOT NULL;
+
+-- AlterTable
+ALTER TABLE "Project" ALTER COLUMN "name" DROP NOT NULL;
