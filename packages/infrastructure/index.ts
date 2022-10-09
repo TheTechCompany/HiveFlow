d35f18e018fde79077de7f59a89ec7618343b12c
Ross Leitch <ross@hexhive.io>
2022-10-09 15:54:35 +1300

    Staging' git

--- packages/infrastructure/index.ts
+++ packages/infrastructure/index.ts
@@ -21,7 +21,7 @@ const main = (async () => {
 
     const kubeconfig = stackRef.getOutput('k3sconfig');
     
-    const rootServer = gatewayRef.getOutput('gatewayUrl');
+    const rootServer = gatewayRef.getOutput('internalGatewayUrl');
     const dbUrl = dbRef.getOutput('postgres_name');
     const dbPass = dbRef.getOutput('postgres_pass');
 
